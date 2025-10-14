8f3c15cfce03cf92589c18b9a694d0cca41f3233
Jim Jiracek <james.jiracek@slalom.com>
2025-10-14 12:55:38 -0400

    init plus axios install and mockapi.io data call

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const endpointUrl = "https://68ee7c4fdf2025af7803dc4f.mockapi.io/books";
+
+  const getBooksList = async () => {
+    const response = await axios.get(endpointUrl);
+    console.log("response", JSON.stringify(response.data, null, 3));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
+      <Button title="Get Books" onPress={getBooksList} />
     </View>
   );
 }
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
